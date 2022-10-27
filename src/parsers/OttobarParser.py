a7baa7ba04b4862c38b3cc52b09a29783f97b947
dan@sludgefree.net <dan@sludgefree.net>
2022-10-27 17:36:38 -0400

    Adding error catching for Ottobar, also handling Ottobar events that don't have a year.

--- src/parsers/OttobarParser.py
+++ src/parsers/OttobarParser.py
@@ -1,36 +1,49 @@
 import re
 from CalendarParser import CalendarParser
 from Event import Event
+from CalendarLogger import logger
+
 
 class OttobarParser(CalendarParser):
 
     def parseEvents(self, settings = {}):
         for eventHtml in self.soup().find_all('div', class_ = 'eventWrapper'):
-            primaryButtonHhtml = eventHtml.find_all('a', class_ = 'btn-primary')
-            if (primaryButtonHhtml):
-                event = Event()
+            try:
+                primaryButtonHhtml = eventHtml.find_all('a', class_ = 'btn-primary')
+                if (primaryButtonHhtml):
+                    event = Event()
 
-                description = self.replaceWhitespaceWithPipes(eventHtml.get_text())
-                event.setDescription(description)
+                    description = self.replaceWhitespaceWithPipes(eventHtml.get_text())
+                    event.setDescription(description)
 
-                title = eventHtml.find('h2')
-                event.setSummary(title.get_text())
+                    title = eventHtml.find('h2')
+                    event.setSummary(title.get_text())
 
-                locationHtml = eventHtml.find('a', class_ = 'venueLink')
-                event.setLocation(locationHtml.get_text())
+                    locationHtml = eventHtml.find('a', class_ = 'venueLink')
+                    event.setLocation(locationHtml.get_text())
 
-                dateHtml = eventHtml.find('div', class_ = 'singleEventDate')
-                date = dateHtml.get_text()
-                timeHtml = eventHtml.find('div', class_ = 'eventDoorStartDate')
-                time = re.findall('(\d+)(:\d\d)?(am|pm)', timeHtml.get_text())[0]
-                event.setStartString(self.buildStartstamp(date, time), '\n%a, %b %d, %Y %I:%M%p')
-                event.setAbsoluteEndDateTime(23, 59)
+                    dateHtml = eventHtml.find('div', class_ = 'singleEventDate')
+                    date = dateHtml.get_text()
+                    timeHtml = eventHtml.find('div', class_ = 'eventDoorStartDate')
+                    time = re.findall('(\d+)(:\d\d)?(am|pm)', timeHtml.get_text())[0]
 
-                link = eventHtml.find('a').get('href')
-                event.setLink(link)
-                if (link):
-                    self.addEvent(event)
+                    try:
+                        event.setStartString(self.buildStartstamp(date, time), '\n%a, %b %d, %Y %I:%M%p')
+                    except:
 
+                        # Sometimes the date doesn't have a year
+                        year = event.getNearestYear(date, '\n%a, %b %d')
+                        date = date + ", " + str(year)
+                        event.setStartString(self.buildStartstamp(date, time), '\n%a, %b %d, %Y %I:%M%p')
+
+                    event.setAbsoluteEndDateTime(23, 59)
+
+                    link = eventHtml.find('a').get('href')
+                    event.setLink(link)
+                    if (link):
+                        self.addEvent(event)
+            except Exception as e:
+                logger.exception("Exception occurred")
         return self
 
     def buildStartstamp(self, date, timePattern):
