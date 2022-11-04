448d4cfd7f9e50f5714eb66d20d9bce00187982c
dan@sludgefree.net <dan@sludgefree.net>
2022-11-04 10:27:16 -0400

    Add a file logging handler that only maps the most recent run.

--- src/CalendarLogger.py
+++ src/CalendarLogger.py
@@ -19,18 +19,24 @@ def buildLogger(options):
 
     c_handler = logging.StreamHandler()
     f_handler = logging.FileHandler(options.log_file)
+    last_handler = logging.FileHandler('./data/current.log', mode = 'w')
+
     logger.setLevel(logging.DEBUG)
     c_handler.setLevel(streamLevel)
+    last_handler.setLevel(logging.ERROR)
     f_handler.setLevel(logging.DEBUG)
 
     # Create formatters and add it to handlers
     c_format = logging.Formatter('%(levelname)s - %(message)s')
+    last_format = logging.Formatter('%(levelname)s - %(message)s')
     f_format = logging.Formatter('%(levelname)s (%(asctime)s) %(message)s')
     c_handler.setFormatter(c_format)
+    last_handler.setFormatter(last_format)
     f_handler.setFormatter(f_format)
 
     # Add handlers to the logger
     logger.addHandler(c_handler)
+    logger.addHandler(last_handler)
     logger.addHandler(f_handler)
 
 def addLoggerArgsToParser(parser):
