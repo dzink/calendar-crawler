feaafcf6d0f5b8f06d1e73741589c659bee751b3
dan@sludgefree.net <dan@sludgefree.net>
2023-03-03 07:13:36 -0500

    More linting - CalendarSource.

--- src/CalendarSource.py
+++ src/CalendarSource.py
@@ -13,35 +13,48 @@ class CalendarSource:
     events = []
     driver = None
 
-    def __init__(self, url, id = '', remote = True):
+    def __init__(self, url, source_id = '', remote = True):
         self.url = url
-        if (id == ''):
-            id = url
-        self.id = id
+        if (source_id == ''):
+            source_id = url
+        self.source_id = source_id
         self.remote = remote
         self.scrollCount = 0
 
-    """
-    Build and return a headless web driver.
-    The driver is a class variable so each source instance will share a driver.
-    """
     def getDriver(self):
-        if (self.driver == None):
+        """
+        Build and return a headless web driver.
+        The driver is a class variable so each source instance will share a driver.
+        """
+        if (self.driver is None):
             chromeOptions = Options()
             chromeOptions.add_argument("--headless")
             chromeOptions.add_argument("--window-size=1920x1080")
-            self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), chrome_options = chromeOptions, executable_path='/usr/bin/google-chrome')
+            self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+                chrome_options = chromeOptions, executable_path='/usr/bin/google-chrome')
             logger.debug('built chrome driver')
         return self.driver
 
     def getHtml(self):
+        """
+        Get the HTML, either remotely or from a local file.
+        This functionality is set from the command line.
+        """
+
         logger.info('retrieving ' + self.url)
+        # I think the else improves readability.
+        # pylint: disable=no-else-return
+
         if (self.remote):
             return self.getRemoteHtml()
         else:
-            return self.getLocalHtml()
+            html = self.getLocalHtml()
+            self.write_local_html(html)
+            return html
 
     def getRemoteHtml(self):
+        """ Get a webdriver and get the remote HTML. """
+
         driver = self.getDriver()
         wait = WebDriverWait(driver, 2)
         driver.get(self.url)
@@ -50,40 +63,40 @@ class CalendarSource:
         self.scrollPage(driver)
 
         if get_url == self.url:
-            page_source_was = ''
             page_source = driver.page_source
-
-        with open(self.pageSourceFilename(), 'w') as token:
-            token.write(page_source)
 
         logger.debug('retrieved remote page source for ' + self.url)
         return page_source
 
+    def write_local_html(self, html):
+        """ Store the html from a remote source pull for faster re-running. """
+        with open(self.local_html_filename(), 'w') as token:
+            token.write(html)
+
     def getLocalHtml(self):
+        """ Get the stored local HTML from the last remote pull. """
         page_source = ''
-        with open(self.pageSourceFilename(), 'r') as token:
+        with open(self.local_html_filename(), 'r') as token:
             page_source = token.read()
             token.close()
         logger.debug('retrieved local page source for ' + self.url)
         return page_source
 
-    def pageSourceFilename(self):
-        return 'source_html/page_source__' + self.id + '.html'
+    def local_html_filename(self):
+        """ Helper to make building the local filename easier. """
+        return 'source_html/page_source__' + self.source_id + '.html'
 
-    """
-    Indicate how many times the page should be scrolled.
-    Useful for pages that auto-load more events.
-    Set this before getting the source.
-    """
     def setScrollCount(self, scrollCount):
+        """
+        Indicate how many times the page should be scrolled.
+        Useful for pages that auto-load more events.
+        Set this before getting the source.
+        """
         self.scrollCount = scrollCount
 
-    """
-    Some sources need to be scrolled to the bottom of the page before they will
-    load all events.
-    """
     def scrollPage(self, driver, pause = 2):
+        """ Some sources need to be scrolled to find all events. This does that. """
         if (self.scrollCount):
-            for i in range(self.scrollCount):
+            for _ in range(self.scrollCount):
                 driver.execute_script("window.scrollBy(0, document.body.scrollHeight);")
                 sleep(pause)
