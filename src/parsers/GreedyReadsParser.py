f6bc1728a499de9182db6fb7fc50ef3b651fa826
dan@sludgefree.net <dan@sludgefree.net>
2025-01-23 10:57:04 -0500

    Updating GreedyReeds parser to fit their new website format.

--- src/parsers/GreedyReadsParser.py
+++ src/parsers/GreedyReadsParser.py
@@ -6,25 +6,26 @@ class GreedyReadsParser(CalendarParser):
 
     def parseEvents(self, html, settings = {}):
         title = 'Unknown event'
-        eventsHtml = self.soup(html).find('div', {'id': 'maintable'})
+        eventsHtml = self.soup(html).find('div', class_ = 'preview-container')
         for eventHtml in eventsHtml.find_all('div', class_ = 'ant-row'):
             try:
-                header = eventHtml.find('h2')
+                header = eventHtml.find('h4')
                 title = header.get_text()
                 event = Event()
 
-                descriptionHtml = eventHtml.find('div', class_ = 'html')
+
+                lines = eventHtml.find_all('div', class_ = 'bm-txt-0')
+                timeStampHtml = lines[0]
+                descriptionHtml = lines[1]
+
                 if descriptionHtml:
                     event.setDescription(descriptionHtml.get_text())
                     linkHtml = descriptionHtml.find('a', href=True)
                     if linkHtml:
                         event.setLink(linkHtml['href'])
 
-
-                timeStampHtml = header.find('span')
                 if timeStampHtml:
                     timeStamp = timeStampHtml.get_text()
-                    title = self.cutPatternFromString(title, timeStamp)
                     date = self.cutPatternFromString(timeStamp, ' @.*')
                     date = self.removeOrdinalsFromNumbersInString(date)
                     startTime, endTime = self.parseStartAndEndTimesFromFuzzyString(timeStamp)
